d6952557592369cf9824e26cda5e2c239eff4e19
Christopher Pickering <christopher@going.bg>
2023-01-03 14:24:59 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,82 @@
+import gzip
+import json
+import os
+import shutil
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_page(url):
+
+    retries = 1
+    while retries < 5:
+        try:
+            return requests.get(url, timeout=30)
+        except Exception as e:
+            wait = retries * 5
+            print(f"Error! Waiting {wait} secs and re-trying {url}...")
+            time.sleep(wait)
+            retries += 1
+
+    raise ValueError("Page could not be loaded")
+
+
+BASE_URL = "https://www.state.gov/international-religious-freedom-reports/"
+
+OUT = {}
+
+p = get_page(BASE_URL)
+
+soup = BeautifulSoup(p.text, "html.parser")
+
+# get the most current report
+current_year_url = soup.find("a", id="latest").parent.find_next_sibling().find('a',href=True).get('href')
+
+p = get_page(f"{current_year_url}")
+
+soup = BeautifulSoup(p.text, "html.parser")
+
+# get list of all country data.
+urls = [
+    *set(
+        [
+            x.get("value")
+            for x in soup.find(
+                "select", class_="field-container chosen-container--country"
+            ).find_all("option")
+            if x.get("value").startswith("http")
+        ]
+    )
+]
+
+# iterate countries and get the region
+for country in urls:
+    print(country)
+    d = get_page(country)
+
+    soup = BeautifulSoup(d.text, "html.parser")
+    country_name = soup.find("title").string.split("-")[0].strip()
+
+    report = soup.find("div", class_="report__content")
+
+    summary = "\n\n".join(
+        [x.text for x in report.find("h2", text="Executive Summary").parent.find_all("p")]
+    )
+
+    OUT[country_name] = {"name": country_name, "summary": summary, "url": country}
+
+    # break
+
+if not os.path.isdir("data"):
+    os.makedirs("data")
+
+json_object = json.dumps(OUT, indent=4)
+with open("data/data.json", "w") as outfile:
+    outfile.write(json_object)
+
+# gzip version
+with open("data/data.json", "rb") as infile:
+    with gzip.open("data/data.json.gz", "wb") as outfile:
+        shutil.copyfileobj(infile, outfile)
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Human Rights Practices
+
+Converts the [USA Department of State Human Rights Practices](https://www.state.gov/reports/2021-country-reports-on-human-rights-practices/) into a json data structure.
+
+## Data
+
+* [Latest JSON File](https://www.github.com/christopherpickering/human_rights_practices/tree/build/data.json) - approx 150 kB - updated monthly
+* [Latest JSON File (gzip)](https://www.github.com/christopherpickering/human_rights_practices/tree/build/data.json.gz) - approx 50 kB - updated monthly
+
+## Building your own file
+
+* clone this repository
+* install deps with `poetry install`
+* run `poetry run python main.py`
+
+A json file called `data.json` will appear.
+
+## License
+
+MIT - see LICENSE
